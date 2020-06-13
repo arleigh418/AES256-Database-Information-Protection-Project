d5758edcc5980132f46459deae194707756a1fbc
張峮瑋(Arleigh Chang) <40189046+arleigh418@users.noreply.github.com>
2020-06-13 13:53:55 +0800

    Update generate_key.py

--- AES256/generate_key.py
+++ AES256/generate_key.py
@@ -1,10 +1,10 @@
 import os
 DEFAULT_ENTROPY = 32
-#隨機產生key
+
 def token_bytes():
     nbytes = DEFAULT_ENTROPY
     return os.urandom(nbytes)
-#讀取control，取得key跟加密後的檔案的絕對路徑
+
 def read_control():
     with open(r'Control.txt',encoding='UTF-8-sig',mode='r') as config:
         config_detail = config.readlines()
@@ -18,7 +18,7 @@ def read_control():
                 result_path = config.split('->')[1]
                 result_path = result_path.replace(r'\n','')
     return key_path,result_path
-#使用tokenbyte跟read_control獲取存檔位置及產生key，並且會移除原新的key及加密後的檔案
+
 def generate_key():
     key_path,result_path = read_control()
     nbytes = DEFAULT_ENTROPY
